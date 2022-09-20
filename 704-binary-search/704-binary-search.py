d80e02af50062357d75aecf1cb9a31f32e6e2e88
Gunjan Lunkad <67200542+gl-24@users.noreply.github.com>
2022-09-20 20:40:10 +0530

    Time: 811 ms (5.02%), Space: 15.5 MB (26.37%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -3,7 +3,7 @@ class Solution:
         start = 0
         end = len(nums)-1
         while start <= end:
-            mid = start + (end - start) // 2
+            mid = (start + end) >> 1
             if nums[mid] == t:
                 return mid
             elif nums[mid] > t:
